be1e3cca775fdffb57be3d07ee1a38160bfe04aa
beepscore <support@beepscore.com>
2013-09-16 15:52:36 -0700

    Add ListViewController. Don't add storyboard or xib file.

---
+++ ClassicPhotos/ListViewController.h
@@ -0,0 +1,13 @@
+//
+//  ListViewController.h
+//  ClassicPhotos
+//
+//  Created by Steve Baker on 9/16/13.
+//  Copyright (c) 2013 Beepscore LLC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ListViewController : UITableViewController
+
+@end
