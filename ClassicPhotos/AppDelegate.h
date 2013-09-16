fe65bf98dbd4ae3faf233cafaa75fbefd5277584
beepscore <support@beepscore.com>
2013-09-16 14:33:53 -0700

    Initial Commit

---
+++ ClassicPhotos/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  ClassicPhotos
+//
+//  Created by Steve Baker on 9/16/13.
+//  Copyright (c) 2013 Beepscore LLC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ ClassicPhotosTests/ClassicPhotosTests.m
@@ -0,0 +1,34 @@
+//
+//  ClassicPhotosTests.m
+//  ClassicPhotosTests
+//
+//  Created by Steve Baker on 9/16/13.
+//  Copyright (c) 2013 Beepscore LLC. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface ClassicPhotosTests : XCTestCase
+
+@end
+
+@implementation ClassicPhotosTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
