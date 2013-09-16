b9a331ff64bb7bfa950b0920f2a22def67474442
beepscore <support@beepscore.com>
2013-09-16 16:13:12 -0700

    Implement viewDidLoad.

--- ClassicPhotos/ListViewController.m
+++ ClassicPhotos/ListViewController.m
@@ -14,6 +14,8 @@
 
 @implementation ListViewController
 
+#pragma mark - Life cycle
+
 - (id)initWithStyle:(UITableViewStyle)style
 {
     self = [super initWithStyle:style];
@@ -26,12 +28,9 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
+    self.title = @"Classic Photos";
+    self.tableView.rowHeight = 80.0;
 
-    // Uncomment the following line to preserve selection between presentations.
-    // self.clearsSelectionOnViewWillAppear = NO;
- 
-    // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-    // self.navigationItem.rightBarButtonItem = self.editButtonItem;
 }
 
 - (void)didReceiveMemoryWarning
