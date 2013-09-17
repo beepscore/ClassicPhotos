776f0c70182b4ebbbf20fc77751fe1ffb4b69ce0
beepscore <support@beepscore.com>
2013-09-16 22:38:35 -0700

    Add class PhotoRecord.

---
+++ ClassicPhotos/PhotoRecord.m
@@ -0,0 +1,13 @@
+//
+//  PhotoRecord.m
+//  ClassicPhotos
+//
+//  Created by Steve Baker on 9/16/13.
+//  Copyright (c) 2013 Beepscore LLC. All rights reserved.
+//
+
+#import "PhotoRecord.h"
+
+@implementation PhotoRecord
+
+@end
