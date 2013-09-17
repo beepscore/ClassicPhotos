60ebddd9d98f3a8dafd3352f9dc53d506d704937
beepscore <support@beepscore.com>
2013-09-16 23:23:27 -0700

    Add class ImageDownloader as a subclass of NSOperation.

---
+++ ClassicPhotos/ImageDownloader.h
@@ -0,0 +1,13 @@
+//
+//  ImageDownloader.h
+//  ClassicPhotos
+//
+//  Created by Steve Baker on 9/16/13.
+//  Copyright (c) 2013 Beepscore LLC. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface ImageDownloader : NSOperation
+
+@end
---
+++ ClassicPhotos/ImageDownloader.m
@@ -0,0 +1,13 @@
+//
+//  ImageDownloader.m
+//  ClassicPhotos
+//
+//  Created by Steve Baker on 9/16/13.
+//  Copyright (c) 2013 Beepscore LLC. All rights reserved.
+//
+
+#import "ImageDownloader.h"
+
+@implementation ImageDownloader
+
+@end
