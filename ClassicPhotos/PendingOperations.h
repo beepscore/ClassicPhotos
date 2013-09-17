1069502910bfbb4cad6c803fee179c7fab4a3d73
beepscore <support@beepscore.com>
2013-09-16 23:01:05 -0700

    Add class PendingOperations.

---
+++ ClassicPhotos/PendingOperations.h
@@ -0,0 +1,13 @@
+//
+//  PendingOperations.h
+//  ClassicPhotos
+//
+//  Created by Steve Baker on 9/16/13.
+//  Copyright (c) 2013 Beepscore LLC. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface PendingOperations : NSObject
+
+@end
---
+++ ClassicPhotos/PendingOperations.m
@@ -0,0 +1,13 @@
+//
+//  PendingOperations.m
+//  ClassicPhotos
+//
+//  Created by Steve Baker on 9/16/13.
+//  Copyright (c) 2013 Beepscore LLC. All rights reserved.
+//
+
+#import "PendingOperations.h"
+
+@implementation PendingOperations
+
+@end
