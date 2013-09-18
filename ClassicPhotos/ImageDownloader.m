e2f15a2f3c819e9cda97eb15d8af8950924c45ca
beepscore <support@beepscore.com>
2013-09-17 20:06:03 -0700

    Implement ImageDownloader.

--- ClassicPhotos/ImageDownloader.m
+++ ClassicPhotos/ImageDownloader.m
@@ -8,6 +8,60 @@
 
 #import "ImageDownloader.h"
 
+@interface ImageDownloader ()
+    // change to readwrite
+@property (nonatomic, readwrite, strong) NSIndexPath *indexPathInTableView;
+@property (nonatomic, readwrite, strong) PhotoRecord *photoRecord;
+@end
+
 @implementation ImageDownloader
+
+#pragma mark - Life Cycle
+
+- (id)initWithPhotoRecord:(PhotoRecord *)record atIndexPath:(NSIndexPath *)indexPath delegate:(id<ImageDownloaderDelegate>)theDelegate {
+
+    if (self = [super init]) {
+        // 2
+        self.delegate = theDelegate;
+        self.indexPathInTableView = indexPath;
+        self.photoRecord = record;
+    }
+    return self;
+}
+
+#pragma mark - Downloading image
+- (void)main {
+
+    @autoreleasepool {
+
+        // Check isCancelled regularly, in order to stop promptly.
+        if (self.isCancelled)
+            return;
+
+        NSData *imageData = [[NSData alloc] initWithContentsOfURL:self.photoRecord.URL];
+
+        if (self.isCancelled) {
+            imageData = nil;
+            return;
+        }
+
+        if (imageData) {
+            UIImage *downloadedImage = [UIImage imageWithData:imageData];
+            self.photoRecord.image = downloadedImage;
+        }
+        else {
+            self.photoRecord.failed = YES;
+        }
+
+        imageData = nil;
+
+        if (self.isCancelled)
+            return;
+
+        // Cast delegate from id<ImageDownloaderDelegate> to NSObject in order to call method performSelectorOnMainThread
+        [(NSObject *)self.delegate performSelectorOnMainThread:@selector(imageDownloaderDidFinish:)
+                                                    withObject:self waitUntilDone:NO];
+    }
+}
 
 @end
