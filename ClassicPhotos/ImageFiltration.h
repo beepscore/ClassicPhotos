bba225d044f117a725a0eb65130a2704562f875a
beepscore <support@beepscore.com>
2013-09-17 20:08:42 -0700

    Add class ImageFiltration as a subclass of NSOperation.

---
+++ ClassicPhotos/ImageFiltration.h
@@ -0,0 +1,13 @@
+//
+//  ImageFiltration.h
+//  ClassicPhotos
+//
+//  Created by Steve Baker on 9/17/13.
+//  Copyright (c) 2013 Beepscore LLC. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface ImageFiltration : NSOperation
+
+@end
---
+++ ClassicPhotos/ImageFiltration.m
@@ -0,0 +1,13 @@
+//
+//  ImageFiltration.m
+//  ClassicPhotos
+//
+//  Created by Steve Baker on 9/17/13.
+//  Copyright (c) 2013 Beepscore LLC. All rights reserved.
+//
+
+#import "ImageFiltration.h"
+
+@implementation ImageFiltration
+
+@end
