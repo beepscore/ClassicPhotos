8d3ddbe4451c36ebef0943deafa5b2b8daf90a58
beepscore <support@beepscore.com>
2013-09-22 16:13:06 -0700

    Delete commented out method.

--- ClassicPhotos/ListViewController.m
+++ ClassicPhotos/ListViewController.m
@@ -246,16 +246,4 @@
     [self.pendingOperations.filtrationsInProgress removeObjectForKey:indexPath];
 }
 
-/*
-#pragma mark - Navigation
-
-// In a story board-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender
-{
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
-}
-
- */
-
 @end
